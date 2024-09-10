4a9b3742f18ec3c65f9ad6ef0e0d61bf2304013f
HaoXK <haoxuank@nycstudents.net>
2024-09-10 18:20:12 -0400

    Add files via upload

---
+++ New.java
@@ -0,0 +1,15 @@
+//1.2 Variables and Data Types
+public class New 
+{
+    public static void main(String[] args) 
+    {
+        String letter = "A";
+        int x = 5;
+        double y = 8.5;
+        boolean z = true;
+        y = y - 1;
+        final int B = 2;
+        System.out.println(letter + B); 
+        System.out.print(x + y + " " + z);
+    }
+}
